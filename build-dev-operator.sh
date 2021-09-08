21beb9e536e53afc024a92bc9dfed9bb4f2d8b4b
Owais Lone <owais@lone.pw>
2021-09-08 05:37:43 +0530

    Updated service account and disabled /metrics auth
    - service account is now called "splunk-otel-account"
    - disabler auth on /metrics endpoint and removed kube proxy sidecar
    - updated build-dev-operator.sh to allow 100% local workflow

--- build-dev-operator.sh
+++ build-dev-operator.sh
@@ -7,7 +7,8 @@ VERSION=0.33.${VERSION:-$(date +%s)}
 BUNDLE_VERSION=${BUNDLE_VERSION:-$VERSION}
 OPERATOR_VERSION=${BUNDLE_VERSION:-$OPERATOR_VERSION}
 
-export USER=${QUAY_USERNAME:-signalfx}
+export IMG_PREFIX=quay.io/$QUAY_USERNAME
+export USER=$QUAY_USERNAME
 export IMG=quay.io/$USER/splunk-otel-operator:v$OPERATOR_VERSION
 export BUNDLE_IMG=quay.io/$USER/splunk-otel-operator-bundle:v$BUNDLE_VERSION
 
@@ -17,28 +18,29 @@ build() {
 	make container
 	make bundle VERSION=${OPERATOR_VERSION}
 	make bundle-build VERSION=${OPERATOR_VERSION}
+    make release-artifacts
 }
 
-build_bundle() {
-	make bundle VERSION=${OPERATOR_VERSION}
+
+load() {
+	kind load docker-image $IMG
+	kind load docker-image $BUNDLE_IMG
+}
+
+
+install() {
+    make cert-manager
+    kubectl apply -f dist/splunk-otel-operator.yaml
 }
 
 publish() {
-	kind load docker-image $IMG
-	kind load docker-image $BUNDLE_IMG
-
 	docker push $IMG
 	docker push $BUNDLE_IMG
-
 	echo "==== Published the following images:"
 	echo $BUNDLE_IMG
 	echo $IMG 
 }
 
-build_publish() {
-	build
-	publish
-}
 
 deploy_local() {
 	kubens operators
@@ -47,4 +49,8 @@ deploy_local() {
 	make deploy
 }
 
-"$@"
+
+for arg; do
+   "$arg"
+done
+
