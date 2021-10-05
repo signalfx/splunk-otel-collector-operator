a45d2f78b6e5f97fc44a471fa4066af4e8559c81
Tomasz Rojek <me@tomrosystems.com>
2021-10-05 14:36:26 +0200

    Initial fix unit tests (#11)
    * Fix for unit tests
    
    * Update bundle
    
    * Add debug to Makefile
    
    * Fix for auto-generated code
    
    * Change default user to signalfx
    
    * Merge bundle

--- pkg/collector/reconcile/configmap.go
+++ pkg/collector/reconcile/configmap.go
@@ -37,25 +37,16 @@ func ConfigMaps(ctx context.Context, params Params) error {
 	desired := []corev1.ConfigMap{}
 
 	if !params.Instance.Spec.Agent.Disabled {
-		if cm, err := desiredConfigMap(ctx, params, params.Instance.Spec.Agent.Config, "agent"); err == nil {
-			desired = append(desired, cm)
-		} else {
-			return err
-		}
+		cm := desiredConfigMap(ctx, params, params.Instance.Spec.Agent.Config, "agent")
+		desired = append(desired, cm)
 	}
 	if !params.Instance.Spec.ClusterReceiver.Disabled {
-		if cm, err := desiredConfigMap(ctx, params, params.Instance.Spec.ClusterReceiver.Config, "cluster-receiver"); err == nil {
-			desired = append(desired, cm)
-		} else {
-			return err
-		}
+		cm := desiredConfigMap(ctx, params, params.Instance.Spec.ClusterReceiver.Config, "cluster-receiver")
+		desired = append(desired, cm)
 	}
 	if !params.Instance.Spec.Gateway.Disabled {
-		if cm, err := desiredConfigMap(ctx, params, params.Instance.Spec.Gateway.Config, "gateway"); err == nil {
-			desired = append(desired, cm)
-		} else {
-			return err
-		}
+		cm := desiredConfigMap(ctx, params, params.Instance.Spec.Gateway.Config, "gateway")
+		desired = append(desired, cm)
 	}
 
 	// first, handle the create/update parts
@@ -71,7 +62,7 @@ func ConfigMaps(ctx context.Context, params Params) error {
 	return nil
 }
 
-func desiredConfigMap(_ context.Context, params Params, config, kind string) (corev1.ConfigMap, error) {
+func desiredConfigMap(_ context.Context, params Params, config, kind string) corev1.ConfigMap {
 	name := naming.ConfigMap(params.Instance, kind)
 	labels := collector.Labels(params.Instance)
 	labels["app.kubernetes.io/name"] = name
@@ -86,7 +77,7 @@ func desiredConfigMap(_ context.Context, params Params, config, kind string) (co
 		Data: map[string]string{
 			"collector.yaml": config,
 		},
-	}, nil
+	}
 }
 
 func expectedConfigMaps(ctx context.Context, params Params, expected []corev1.ConfigMap, retry bool) error {
